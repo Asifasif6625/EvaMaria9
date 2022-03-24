d135770edfa23c35ed95f0851ea5244d6ece3ef8
Asif A.S <86577020+Asifasif6625@users.noreply.github.com>
2022-03-24 11:33:06 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -315,13 +315,12 @@ async def cb_handler(client: Client, query: CallbackQuery):
             f_caption=f_caption
         if f_caption is None:
             f_caption = f"{files.file_name}"
-         buttons =  [ [
-            await query.answer('file foewed ചെയ്ത ശേഷം താഴെ കാണുന്ന button click ചെയ്യുക.',show_alert = True)
-                InlineKeyboardButton('🧼𝐃𝐞𝐥𝐞𝐭𝐞 𝐭𝐡𝐢𝐬 𝐦𝐞𝐬𝐬𝐚𝐠𝐞🧼', callback_data='help')
-                ] ]  
+         buttons =  [[
+                InlineKeyboardButton('🧼𝐃𝐞𝐥𝐞𝐭𝐞 𝐭𝐡𝐢𝐬 𝐦𝐞𝐬𝐬𝐚𝐠𝐞🧼', callback_data='help', show_alert = True)
+        ]]  
         try:
             username = ab[0]
-        except:
+        except
             pass
         try:
             bot = await client.get_me()
